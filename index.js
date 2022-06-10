9f4d6bdaf75fb72c637d7ab53fef9e7503a1f5b0
miker <mikerz83@gmail.com>
2022-06-10 13:02:59 +0200

    contracts wrappers

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const { generateSmartContractSpecification } = require('./lib/generator');
 const { OffChainStorage } = require('./lib/offChainStorage');
 const { EthereumDeployer } = require('./lib/eth/deployer');
 const { EthereumParser } = require('./lib/eth/parser');
+const {
+  PolicyContractWrapper,
+  PolicyNFTWrapper,
+} = require('./lib/eth/contractWrappers');
 
 module.exports = {
   generateSmartContractSpecification,
   OffChainStorage,
   EthereumDeployer,
   EthereumParser,
+  PolicyContractWrapper,
+  PolicyNFTWrapper,
 };
