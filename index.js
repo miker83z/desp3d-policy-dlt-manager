f880a61438ee1e832d17e7a8660aa5b7202483ce
miker <mikerz83@gmail.com>
2022-06-10 10:33:22 +0200

    small fix

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ const { generateSmartContractSpecification } = require('./lib/generator');
 const { OffChainStorage } = require('./lib/offChainStorage');
 const { EthereumDeployer } = require('./lib/eth/deployer');
 const { EthereumParser } = require('./lib/eth/parser');
-const { AlgoDeployer } = require('./lib/algo/deployer');
-const { AlgoParser } = require('./lib/algo/parser');
 
 module.exports = {
   generateSmartContractSpecification,
   OffChainStorage,
   EthereumDeployer,
   EthereumParser,
-  AlgoDeployer,
-  AlgoParser,
 };
